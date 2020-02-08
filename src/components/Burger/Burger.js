73670c998ce0dc6e78d48f04315106f360d5b3f6
Yichuan Wang <w369371@gmail.com>
2020-02-08 15:30:58 +0800

    ingredients are now passed throu props, update Burger.js, Burger.css

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = props => {
+  const transformedIngredients = Object.keys(props.ingredients).map(key => {
+    var array = [];
+    console.log(props.ingredients[key]);
+    for (let a = 0; a < props.ingredients[key]; a++) {
+      array.push(<BurgerIngredient type={key}></BurgerIngredient>);
+    }
+    return array;
+  });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"></BurgerIngredient>
-      <BurgerIngredient type="cheese"></BurgerIngredient>
-      <BurgerIngredient type="meat"></BurgerIngredient>
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom"></BurgerIngredient>
     </div>
   );
