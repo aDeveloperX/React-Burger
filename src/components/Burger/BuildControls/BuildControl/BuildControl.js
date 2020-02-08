f36d47d4d72a278b514753e1808732cd0130ec35
Yichuan Wang <w369371@gmail.com>
2020-02-08 21:29:02 +0800

    add panel control styling

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,7 +4,7 @@ import classes from "./buildControl.module.css";
 const buildControl = props => {
   <div className={classes.buildControl}>
     <div className={classes.Label}>{props.label}</div>
-    <button>Less</button>
-    <button>more</button>
+    <button className={classes.Less}>Less</button>
+    <button className={classes.More}>more</button>
   </div>;
 };
