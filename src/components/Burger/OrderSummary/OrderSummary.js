3c5a756d2fb0280d2ae64cd8a418650380cddb66
Yichuan Wang <w369371@gmail.com>
2020-02-13 23:58:35 +0800

    udpate ordersummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -20,7 +20,7 @@ class OrderSummary extends Component {
     return (
       <>
         <h3>Your Order</h3>
-        <p>A delicious burger with the following ingredients:</p>
+        <p>A delicious burger contains the following ingredients:</p>
         <ul>{ingredientSummary}</ul>
         <p>Continue to Checkout?</p>
         <p>
